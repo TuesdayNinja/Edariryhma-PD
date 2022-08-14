775faf922ba3f6ce0838802a625a6727d75882d0
Loviisa Hurme <loviisa.hurme@outlook.com>
2022-08-14 19:52:07 +0300

    Add base content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
 import styled, { createGlobalStyle } from "styled-components";
 import ContentBlock from "../components/ContentBlock";
 import Header from "../components/Header";
@@ -67,6 +66,88 @@ const Home: NextPage = () => {
               kaikki edaritoiminnasta kiinnistuneet mukaan kaikista
               ainejärjestöistä!{" "}
             </p>
+          </ContentBlock>
+          <ContentBlock color="white">
+            <div>
+              <h1>Tapa Toimia</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                nec tempor neque. Nullam ligula metus, consectetur ut faucibus
+                in, aliquam et velit. Nam maximus viverra ipsum non mattis.
+                Quisque non purus id lorem finibus varius eu ut erat. Etiam
+                tincidunt vel orci nec venenatis. Sed efficitur, elit non
+                elementum viverra, est erat elementum sem, eu tincidunt elit ex
+                eu metus. Sed condimentum tincidunt sagittis. Sed egestas et
+                massa sit amet sagittis. Praesent porttitor, sapien quis finibus
+                tempor, justo urna tincidunt lectus, quis convallis lorem diam
+                quis metus. Sed eu molestie turpis. Integer eget molestie diam.
+                Nullam vel sapien lacinia, euismod leo sit amet, sagittis nisi.
+                Fusce imperdiet nisi sit amet finibus molestie. Integer
+                hendrerit sem ac nibh lobortis, viverra accumsan metus
+                imperdiet. Nulla tellus nunc, rhoncus eget bibendum eget,
+                facilisis sed risus. Mauris egestas laoreet lorem, ac porttitor
+                nibh lacinia vitae. Integer sagittis massa ut ipsum vehicula
+                placerat sed consectetur ligula. Curabitur eleifend lectus vitae
+                vestibulum vulputate. Phasellus vulputate accumsan viverra.
+                Donec dapibus tortor in odio pellentesque, a faucibus risus
+                volutpat. Donec eget odio turpis. Aliquam et justo malesuada,
+                accumsan urna nec, volutpat erat. Morbi bibendum erat vel eros
+                accumsan, quis consectetur justo finibus. Ut blandit a nibh vel
+                dictum.
+              </p>
+            </div>
+          </ContentBlock>
+          <ContentBlock color="grey">
+            <div>
+              <h1>Edustajat</h1>
+              <p>
+                Pörssi&amp;Dumpin edustajat toimivat useissa eri Jyväskylän
+                yliopiston ja Jyväskylän yliopiston ylioppilaskunnan
+                vaikuttamiselimissä, kuten ylioppilaskunnan edustajistossa ja
+                hallituksessa, yliopistokollegiossa, ylioppilaslehden
+                johtokunnassa jne. Vaikuttamispaikat vaihtelevat jonkin verran
+                vuosittain, mutta kaikilla rintamilla tehdään silti jatkuvaa
+                edunvalvontatyötä P&amp;D:läisten eteen.
+              </p>
+
+              <h2>P&amp;D JYYn hallituksessa</h2>
+
+              <h2>P&amp;D JYYn edustajistossa</h2>
+              <h3>Varsinaiset edustajat</h3>
+              <h3>Vara edustajat</h3>
+
+              <h2>P&amp;D muissa luottamustoimissa</h2>
+            </div>
+          </ContentBlock>
+          <ContentBlock color="red">
+            <div>
+              <h1>Ota yhteyttä</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+                nec tempor neque. Nullam ligula metus, consectetur ut faucibus
+                in, aliquam et velit. Nam maximus viverra ipsum non mattis.
+                Quisque non purus id lorem finibus varius eu ut erat. Etiam
+                tincidunt vel orci nec venenatis. Sed efficitur, elit non
+                elementum viverra, est erat elementum sem, eu tincidunt elit ex
+                eu metus. Sed condimentum tincidunt sagittis. Sed egestas et
+                massa sit amet sagittis. Praesent porttitor, sapien quis finibus
+                tempor, justo urna tincidunt lectus, quis convallis lorem diam
+                quis metus. Sed eu molestie turpis. Integer eget molestie diam.
+                Nullam vel sapien lacinia, euismod leo sit amet, sagittis nisi.
+                Fusce imperdiet nisi sit amet finibus molestie. Integer
+                hendrerit sem ac nibh lobortis, viverra accumsan metus
+                imperdiet. Nulla tellus nunc, rhoncus eget bibendum eget,
+                facilisis sed risus. Mauris egestas laoreet lorem, ac porttitor
+                nibh lacinia vitae. Integer sagittis massa ut ipsum vehicula
+                placerat sed consectetur ligula. Curabitur eleifend lectus vitae
+                vestibulum vulputate. Phasellus vulputate accumsan viverra.
+                Donec dapibus tortor in odio pellentesque, a faucibus risus
+                volutpat. Donec eget odio turpis. Aliquam et justo malesuada,
+                accumsan urna nec, volutpat erat. Morbi bibendum erat vel eros
+                accumsan, quis consectetur justo finibus. Ut blandit a nibh vel
+                dictum.
+              </p>
+            </div>
           </ContentBlock>
         </Content>
       </main>
